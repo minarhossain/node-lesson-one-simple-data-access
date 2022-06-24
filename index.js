8253232e6c0394ef6846057226bcfca63b3f6255
minarhossain <codewithmnr@gmail.com>
2022-06-24 19:22:18 +0600

    first commit for node js

---
+++ index.js
@@ -0,0 +1,5 @@
+const {addNumbers} = require('./students');
+// console.log(getName());
+// console.log(profession());
+// console.log(age);
+console.log(addNumbers(2, 3, 4));
---
+++ students.js
@@ -0,0 +1,28 @@
+exports.addNumbers = (one, two, three) => {
+    return (one + two + three);
+}
+
+
+
+/* exports.getName = () => {
+    return "Minar Hossain";
+
+}
+exports.profession = () => {
+    return "Learning Web Development";
+}
+exports.age = "25"; */
+
+// We can exports one by one
+// exports.getName = getName;
+// exports.profession = profession;
+// exports.age = age;
+
+// we can exprots module one
+/* module.exports = {
+    getName,
+    profession,
+    age
+} */
+
+// third step is we can exports directly by declaring (exports.functionName)
